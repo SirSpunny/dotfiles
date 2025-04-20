d94be1254e6d5e2943457f2a4fdd92708067e9fd
Lukas Jannicke <eljot@web.de>
2025-04-20 19:38:45 +0200

    added default pickers for snacks.picker

--- private_dot_config/nvim/lua/plugins/snacks.lua
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -28,6 +28,7 @@ return {
 					pane = 2,
 					icon = " ",
 					title = "Recent Files",
+					key = "[<leader>sr]",
 					section = "recent_files",
 					indent = 2,
 					padding = 2,
@@ -37,6 +38,7 @@ return {
 					pane = 2,
 					icon = " ",
 					title = "Projects",
+					key = "[<leader>sp]",
 					section = "projects",
 					indent = 2,
 					padding = 2,
@@ -48,6 +50,7 @@ return {
 						enabled = in_git,
 						icon = " ",
 						title = "Git Status",
+						key = "[<leader>gf]",
 						section = "terminal",
 						cmd = "git --no-pager diff --stat -B -M -C",
 						pane = 2,
@@ -81,18 +84,189 @@ return {
 			desc = "Smart Find Files",
 		},
 		{
-			"<leader>ef",
+			"<leader>/",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>:",
+			function()
+				Snacks.picker.command_history()
+			end,
+			desc = "Command History",
+		},
+		{
+			"<leader>e",
 			function()
 				Snacks.explorer()
 			end,
-			desc = "Toggle [e]xplore [f]iles",
+			desc = "File Explorer",
 		},
 		{
-			"<leader>ed",
+			"<leader>tt",
+			function()
+				Snacks.terminal()
+			end,
+			desc = "Toggle Terminal",
+		},
+		-- git
+		{
+			"<leader>gb",
+			function()
+				Snacks.picker.git_branches()
+			end,
+			desc = "Git Branches",
+		},
+		{
+			"<leader>gB",
+			function()
+				Snacks.gitbrowse()
+			end,
+			desc = "Git Browse",
+			mode = { "n", "v" },
+		},
+		{
+			"<leader>gg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader>gl",
+			function()
+				Snacks.picker.git_log()
+			end,
+			desc = "Git Log",
+		},
+		{
+			"<leader>gL",
+			function()
+				Snacks.picker.git_log_line()
+			end,
+			desc = "Git Log Line",
+		},
+		{
+			"<leader>gs",
+			function()
+				Snacks.picker.git_status()
+			end,
+			desc = "Git Status",
+		},
+		{
+			"<leader>gS",
+			function()
+				Snacks.picker.git_stash()
+			end,
+			desc = "Git Stash",
+		},
+		{
+			"<leader>gd",
+			function()
+				Snacks.picker.git_diff()
+			end,
+			desc = "Git Diff (Hunks)",
+		},
+		{
+			"<leader>gf",
+			function()
+				Snacks.picker.git_files()
+			end,
+			desc = "Find Git Files",
+		},
+		-- search
+		{
+			"<leader>sd",
 			function()
 				Snacks.picker.diagnostics()
 			end,
-			desc = "Toggle [e]xplore [d]iagnostics",
+			desc = "Diagnostics",
+		},
+		{
+			"<leader>sD",
+			function()
+				Snacks.picker.diagnostics_buffer()
+			end,
+			desc = "Buffer Diagnostics",
+		},
+		{
+			"<leader>sh",
+			function()
+				Snacks.picker.help()
+			end,
+			desc = "Help Pages",
+		},
+		{
+			"<leader>sH",
+			function()
+				Snacks.picker.highlights()
+			end,
+			desc = "Highlights",
+		},
+		{
+			"<leader>sk",
+			function()
+				Snacks.picker.keymaps()
+			end,
+			desc = "Keymaps",
+		},
+		{
+			"<leader>sl",
+			function()
+				Snacks.picker.loclist()
+			end,
+			desc = "Location List",
+		},
+		{
+			"<leader>sm",
+			function()
+				Snacks.picker.marks()
+			end,
+			desc = "Marks",
+		},
+		{
+			"<leader>sM",
+			function()
+				Snacks.picker.man()
+			end,
+			desc = "Man Pages",
+		},
+		{
+			"<leader>sq",
+			function()
+				Snacks.picker.qflist()
+			end,
+			desc = "Quickfix List",
+		},
+		{
+			"<leader>su",
+			function()
+				Snacks.picker.undo()
+			end,
+			desc = "Undo History",
+		},
+		{
+			"<leader>sb",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>sp",
+			function()
+				Snacks.picker.projects()
+			end,
+			desc = "Projects",
+		},
+		{
+			"<leader>sr",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = "Recent Files",
 		},
 	},
 }
