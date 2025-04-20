1a0726818a3075a715edb25c467e9451deae0162
SirSpunny <eljot@web.de>
2025-04-20 18:56:15 +0200

    Feature/taskwarrior integration (#9)
    * added a barebone taskwarrior integraion
    
    * removed taskwarrior integration,since it  will be moved to an own repository

--- private_dot_config/nvim/lua/plugins/snacks.lua
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -8,46 +8,22 @@ return {
 		dashboard = {
 			enabled = true,
 			sections = {
-				function()
-					local in_git = Snacks.git.get_root() ~= nil
-					local cmds = {
-						{
-							icon = " ",
-							title = "Open PRs",
-							cmd = "gh pr list -L 3",
-							key = "P",
-							action = function()
-								vim.fn.jobstart("gh pr list --web", { detach = true })
-							end,
-							height = 7,
-						},
-						{
-							title = "Open Issues",
-							cmd = "gh issue list -L 3",
-							key = "i",
-							action = function()
-								vim.fn.jobstart("gh issue list --web", { detach = true })
-							end,
-							icon = " ",
-							height = 7,
-						},
-						{
-							icon = " ",
-							title = "Git Status",
-							cmd = "git --no-pager diff --stat -B -M -C",
-							height = 10,
-						},
-					}
-					return vim.tbl_map(function(cmd)
-						return vim.tbl_extend("force", {
-							section = "terminal",
-							enabled = in_git,
-							padding = 2,
-							ttl = 5 * 60,
-						}, cmd)
-					end, cmds)
-				end,
-				{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 2 },
+				{ action = "", icon = "! ", title = "Tasks", indent = 2, height = 1 },
+				{
+					section = "terminal",
+					cmd = "task rc.verbose=nothing calendar",
+					indent = 2,
+					padding = 2,
+					height = 8,
+					ttl = 0,
+				},
+				{
+					section = "terminal",
+					cmd = "task rc.verbose=nothing status:pending list",
+					indent = 2,
+					padding = 2,
+					ttl = 0,
+				},
 				{
 					pane = 2,
 					icon = " ",
@@ -55,9 +31,34 @@ return {
 					section = "recent_files",
 					indent = 2,
 					padding = 2,
+					height = 6,
 				},
+				{
+					pane = 2,
+					icon = " ",
+					title = "Projects",
+					section = "projects",
+					indent = 2,
+					padding = 2,
+					height = 6,
+				},
+				function()
+					local in_git = Snacks.git.get_root() ~= nil
+					local cmds = {
+						enabled = in_git,
+						icon = " ",
+						title = "Git Status",
+						section = "terminal",
+						cmd = "git --no-pager diff --stat -B -M -C",
+						pane = 2,
+						height = 6,
+						padding = 2,
+						ttl = 5 * 60,
+					}
 
-				{ pane = 2, section = "startup", padding = 2 },
+					return cmds
+				end,
+				{ section = "startup", padding = 2, height = 1 },
 			},
 		},
 		explorer = { enabled = true },
